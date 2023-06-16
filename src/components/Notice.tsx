bf480ee0df4cd94258b54260a2501ee0ddf1e50b
presentKey <kim.hyeonjae4@gmail.com>
2023-06-17 03:31:29 +0900

    refactor: Notice 컴포넌트 개선

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -2,6 +2,19 @@ import styles from './css/Notice.module.css';
 import CheckIcon from './icons/CheckIcon';
 import ExclamationIcon from './icons/ExclamationIcon';
 
+const notice = [
+  {
+    id: 'note',
+    icon: <ExclamationIcon />,
+    title: 'Note',
+  },
+  {
+    id: 'tip',
+    icon: <CheckIcon />,
+    title: 'Tip',
+  },
+];
+
 type Props = {
   type: 'note' | 'tip';
   text: string;
@@ -10,21 +23,25 @@ type Props = {
 export default function Notice({ type, text }: Props) {
   return (
     <div className={styles.notice}>
-      <div className={`${styles.top} ${styles[getTopStyle(type)]}`}>
-        <span>{type === 'note' ? <ExclamationIcon /> : <CheckIcon />}</span>
-        <h4 className={styles.title}>{type === 'note' ? 'Note' : 'Tip'}</h4>
-      </div>
-      <p className={`${styles.message} ${styles[getMessageStyle(type)]}`}>
+      {notice.map(
+        ({ id, icon, title }) =>
+          id === type && (
+            <div
+              key={id}
+              className={`${styles.top} ${styles[getNoticeStyle(type)]}`}
+            >
+              <span>{icon}</span>
+              <h4 className={styles.title}>{title}</h4>
+            </div>
+          )
+      )}
+      <p className={`${styles.message} ${styles[getNoticeStyle(type)]}`}>
         {text}
       </p>
     </div>
   );
 }
 
-function getTopStyle(type: string) {
-  return type === 'note' ? 'blue' : 'green';
-}
-
-function getMessageStyle(type: string) {
+function getNoticeStyle(type: string) {
   return type === 'note' ? 'blue' : 'green';
 }
