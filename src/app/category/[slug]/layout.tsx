952255dfe8470740afa23dd7a5d8e5937547a5e4
presentKey <kim.hyeonjae4@gmail.com>
2023-06-25 19:25:56 +0900

    feat: SideNav 마크업

--- src/app/category/[slug]/layout.tsx
+++ src/app/category/[slug]/layout.tsx
@@ -1,11 +1,16 @@
+import SideNav from '@/components/SideNav';
+import { service } from '@/service/pickService';
+
 type Props = {
   children: React.ReactNode;
 };
 
-export default function CategoryLayout({ children }: Props) {
+export default async function CategoryLayout({ children }: Props) {
+  const categories = await service.category.getAllMainCategory();
+
   return (
     <>
-      <section>사이드 nav</section>
+      <SideNav categories={categories} />
       {children}
     </>
   );
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,21 @@
+import { MainCategory } from '@/model/category';
+import CategoryCard from './CategoryCard';
+import styles from './css/SideNav.module.css';
+
+type Props = {
+  categories: MainCategory[];
+};
+
+export default function SideNav({ categories }: Props) {
+  return (
+    <nav className={`sm-hidden ${styles.nav}`}>
+      <ol>
+        {categories.map((category) => (
+          <li className={styles.list} key={category.path}>
+            <CategoryCard category={category} direction='row' />
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
