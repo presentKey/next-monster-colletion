e5104e76a29861a3fea048cf1e5265284cb22134
presentKey <kim.hyeonjae4@gmail.com>
2023-07-01 23:54:12 +0900

    feat: SideNav open/close 토글 버튼 구현

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,6 +3,7 @@ import { MainCategory } from '@/model/category';
 import CategoryCard from './CategoryCard';
 import styles from './css/SideNav.module.css';
 import ArrowBarIcon from './icons/ArrowBarIcon';
+import { useState } from 'react';
 
 type Props = {
   categories: MainCategory[];
@@ -10,24 +11,32 @@ type Props = {
 };
 
 export default function SideNav({ categories, param }: Props) {
-  return (
-    <nav className={`sm-hidden ${styles.nav}`}>
-      <ol className={styles.list}>
-        {categories.map((category) => (
-          <li className={styles.item} key={category.path}>
-            <CategoryCard
-              category={category}
-              param={param}
-              direction='row'
-              imgSize='small'
-            />
-          </li>
-        ))}
+  const [open, setOpen] = useState(true);
+  const handleToggle = () => setOpen((prev) => !prev);
 
-        <button className={styles['toggle-button']} type='button'>
-          <ArrowBarIcon />
-        </button>
+  return (
+    <nav className={`sm-hidden ${styles.nav} ${open && styles['is-open']}`}>
+      <ol>
+        {open &&
+          categories.map((category) => (
+            <li className={styles.item} key={category.path}>
+              <CategoryCard
+                category={category}
+                param={param}
+                direction='row'
+                imgSize='small'
+              />
+            </li>
+          ))}
       </ol>
+      <button
+        className={styles['toggle-button']}
+        type='button'
+        onClick={handleToggle}
+        title={`${open ? '카테고리 닫기' : '카테고리 열기'}`}
+      >
+        <ArrowBarIcon />
+      </button>
     </nav>
   );
 }
