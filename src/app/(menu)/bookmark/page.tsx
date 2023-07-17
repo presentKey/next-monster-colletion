f8fd1c88d0daba3a22cf3459f5b7a95fbd715284
presentKey <kim.hyeonjae4@gamil.com>
2023-07-18 00:16:33 +0900

    design: 북마크 페이지 Headline 컴포넌트 적용

--- src/app/(menu)/bookmark/page.tsx
+++ src/app/(menu)/bookmark/page.tsx
@@ -1,5 +1,12 @@
 import MemberBookmark from '@/components/BookmarkPage/MemberBookmark';
+import Headline from '@/components/common/Headline/Headline';
+import styles from './page.module.css';
 
 export default function BookmarkPage() {
-  return <MemberBookmark />;
+  return (
+    <div className={styles.container}>
+      <Headline title='북마크' />
+      <MemberBookmark />
+    </div>
+  );
 }
