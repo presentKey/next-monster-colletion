2956729f49b4d9d29926c941e55477b9b62d471a
presentKey <kim.hyeonjae4@gamil.com>
2023-07-20 00:56:22 +0900

    feat: sanity cdn 설정을 false로 변경
    - sanity cdn 설정이 true인 경우, 데이터 읽기/쓰기에 약간의 딜레이가 발생

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2023-06-17',
   token: process.env.SANITY_SECRET_TOKEN,
 });
