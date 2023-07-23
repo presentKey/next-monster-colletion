e4170a3d066694e3c28a6ff12e3a552f3c7134ff
presentKey <kim.hyeonjae4@gmail.com>
2023-07-24 01:01:45 +0900

    design: 데스크탑 timerbar 스타일 적용

--- src/components/layout/TimerBar/TimerBar.tsx
+++ src/components/layout/TimerBar/TimerBar.tsx
@@ -12,25 +12,29 @@ export default function TimerBar() {
 
   return (
     <>
-      <aside className={`${styles.timerbar} ${open && styles['is-open']}`}>
-        <header className={styles.header}>
-          <h3>타이머</h3>
-          <button
-            className={styles.close}
-            type='button'
-            onClick={toggleTimerBar}
-          >
-            <CloseIcon />
-          </button>
-        </header>
+      {timers.length > 0 && (
+        <aside className={`${styles.timerbar} ${open && styles['is-open']}`}>
+          <header className={`sm-only ${styles.header}`}>
+            <h3>타이머</h3>
+            <button
+              className={styles.close}
+              type='button'
+              onClick={toggleTimerBar}
+            >
+              <CloseIcon />
+            </button>
+          </header>
 
-        <ul>
-          {timers.map((timer) => (
-            <Timer key={timer.monsterName} timer={timer} />
-          ))}
-        </ul>
-      </aside>
-      {open && <BackgroundOverlay onClose={toggleTimerBar} />}
+          <ul className={styles.list}>
+            {timers.map((timer) => (
+              <Timer key={timer.monsterName} timer={timer} />
+            ))}
+          </ul>
+        </aside>
+      )}
+      {open && timers.length > 0 && (
+        <BackgroundOverlay onClose={toggleTimerBar} />
+      )}
     </>
   );
 }
