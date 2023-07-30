dbc38759a4b268d5d4898689600f0ac5cc8f3960
presentKey <kim.hyeonjae4@gmail.com>
2023-07-30 23:58:59 +0900

    feat: 기본 엘리트 몬스터 보여주기

--- src/app/(menu)/elite/page.tsx
+++ src/app/(menu)/elite/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <div>엘몬</div>;
+import MonsterCardList from '@/components/common/MonsterCardList/MonsterCardList';
+import { service } from '@/service/pickService';
+
+export default async function ElitePage() {
+  const eliteMonsters = await service.elite.getElite();
+
+  return (
+    <div>
+      <MonsterCardList monsters={eliteMonsters} type='elite' />
+    </div>
+  );
 }
--- src/components/common/MonsterCardList/MonsterCardList.tsx
+++ src/components/common/MonsterCardList/MonsterCardList.tsx
@@ -1,20 +1,25 @@
-import { Monster } from '@/model/monster';
+import { EliteMonster, Monster } from '@/model/monster';
 import styles from './css/MonsterCardList.module.css';
 import MonsterCard from '../MonsterCard/MonsterCard';
 import { Suspense } from 'react';
 
 type Props = {
-  monsters: Monster[];
+  monsters: Monster[] | EliteMonster[];
+  type?: 'general' | 'elite';
 };
 
-export default function MonsterCardList({ monsters }: Props) {
+export default function MonsterCardList({ monsters, type = 'general' }: Props) {
   return (
     <ol className={styles.item}>
       {monsters.map((monster, index) => (
         <li className={styles.list} key={index}>
-          <Suspense fallback={<></>}>
-            <MonsterCard monster={monster} />
-          </Suspense>
+          {type === 'general' ? (
+            <Suspense fallback={<></>}>
+              <MonsterCard monster={monster} />
+            </Suspense>
+          ) : (
+            <p>{monster.name}</p>
+          )}
         </li>
       ))}
     </ol>
