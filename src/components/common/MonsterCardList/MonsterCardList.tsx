01c9206706fd9f386b17940004c3d4072da217d1
presentKey <kim.hyeonjae4@gmail.com>
2023-07-31 22:57:40 +0900

    feat: MonsterCardList 변경 취소

--- src/components/common/MonsterCardList/MonsterCardList.tsx
+++ src/components/common/MonsterCardList/MonsterCardList.tsx
@@ -1,26 +1,20 @@
-import { EliteMonster, Monster } from '@/model/monster';
+import { Monster } from '@/model/monster';
 import styles from './css/MonsterCardList.module.css';
 import MonsterCard from '../MonsterCard/MonsterCard';
 import { Suspense } from 'react';
-import EliteMonsterCard from '@/components/ElitePage/EliteMonsterCard/EliteMonsterCard';
 
 type Props = {
-  monsters: Monster[] | EliteMonster[];
-  type?: 'general' | 'elite';
+  monsters: Monster[];
 };
 
-export default function MonsterCardList({ monsters, type = 'general' }: Props) {
+export default function MonsterCardList({ monsters }: Props) {
   return (
     <ol className={styles.item}>
       {monsters.map((monster, index) => (
         <li className={styles.list} key={index}>
-          {type === 'general' ? (
-            <Suspense fallback={<></>}>
-              <MonsterCard monster={monster as Monster} />
-            </Suspense>
-          ) : (
-            <EliteMonsterCard monster={monster as EliteMonster} />
-          )}
+          <Suspense fallback={<></>}>
+            <MonsterCard monster={monster as Monster} />
+          </Suspense>
         </li>
       ))}
     </ol>
