798810c1bb44f8881857ce9f9eb789d12f8b3079
presentKey <kim.hyeonjae4@gmail.com>
2023-07-31 22:54:23 +0900

    feat: EliteMonsterCardList 구현

--- src/app/(menu)/elite/page.tsx
+++ src/app/(menu)/elite/page.tsx
@@ -1,12 +1,13 @@
-import MonsterCardList from '@/components/common/MonsterCardList/MonsterCardList';
 import { service } from '@/service/pickService';
+import styles from './page.module.css';
+import EliteMonsterCardList from '@/components/ElitePage/EliteMonsterCardList/EliteMonsterCardList';
 
 export default async function ElitePage() {
-  const eliteMonsters = await service.elite.getElite();
+  const eliteMonsters = await service.elite.getDefaultElite();
 
   return (
-    <div>
-      <MonsterCardList monsters={eliteMonsters} type='elite' />
+    <div className={styles.container}>
+      <EliteMonsterCardList monsters={eliteMonsters} />
     </div>
   );
 }
---
+++ src/components/ElitePage/EliteMonsterCardList/EliteMonsterCardList.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { DefaultEliteCollections } from '@/model/monster';
+import EliteMonsterCard from '../EliteMonsterCard/EliteMonsterCard';
+import styles from './css/EliteMonsterCardList.module.css';
+import { useSession } from 'next-auth/react';
+import { getUserEliteCollections } from '@/service/request/eliteCollection';
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from '@/components/common/LoadingSpinner/LoadingSpinner';
+
+type Props = {
+  monsters: DefaultEliteCollections[];
+};
+
+export default function EliteMonsterCardList({ monsters }: Props) {
+  const { data: session } = useSession();
+  const { isLoading, data: myElite } = useQuery(
+    ['myCollection', session?.user.uid],
+    () => getUserEliteCollections(),
+    {
+      staleTime: 1000 * 60 * 60,
+      enabled: !!session,
+    }
+  );
+  const eliteMonsters = myElite?.eliteCollections ?? monsters;
+
+  if (isLoading) return <LoadingSpinner />;
+  return (
+    <ol className={styles.item}>
+      {eliteMonsters.map((monster) => (
+        <li className={styles.list} key={monster.elite.name}>
+          <EliteMonsterCard monster={monster.elite} />
+        </li>
+      ))}
+    </ol>
+  );
+}
