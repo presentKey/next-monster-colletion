f51b77a3c04224bc7dd56248bd8bdc0ae1d9c3cd
presentKey <kim.hyeonjae4@gmail.com>
2023-08-04 03:33:41 +0900

    design: CheckButton 스타일 적용

--- src/components/ElitePage/CheckButton/CheckButton.tsx
+++ src/components/ElitePage/CheckButton/CheckButton.tsx
@@ -1,3 +1,5 @@
+import styles from './css/CheckButton.module.css';
+
 type Props = {
   id: string;
   check: boolean;
@@ -6,14 +8,17 @@ type Props = {
 
 export default function CheckButton({ id, check, onChange }: Props) {
   return (
-    <div>
+    <div className={styles.button} role='button'>
       <input
+        className={styles.check}
         type='checkbox'
         id={id}
         checked={check}
         onChange={(e) => onChange(e)}
       />
-      <label htmlFor={id}>수식어 보기</label>
+      <label className={styles.label} htmlFor={id}>
+        수식어 보기
+      </label>
     </div>
   );
 }
