fc39b5ea7f2d7df80675fa5e12332267f5844a57
presentKey <kim.hyeonjae4@gmail.com>
2023-08-04 23:30:30 +0900

    feat: 활성화된 tab 목록 상태 구현

--- src/components/layout/SubCategoryTab/SubCategoryTab.tsx
+++ src/components/layout/SubCategoryTab/SubCategoryTab.tsx
@@ -3,13 +3,16 @@ import { SubCategory } from '@/model/category';
 import styles from './css/SubCategoryTab.module.css';
 import { useRecoilValue } from 'recoil';
 import { timerListLength } from '@/recoil/TimerBar/selectors';
+import { useState } from 'react';
 
 type Props = {
   subCategories: SubCategory[];
 };
 
 export default function SubCategoryTab({ subCategories }: Props) {
+  const [active, setActive] = useState(0);
   const timerLength = useRecoilValue(timerListLength);
+  const handleActiveTabClick = (index: number) => setActive(index);
 
   return (
     <aside
@@ -18,8 +21,11 @@ export default function SubCategoryTab({ subCategories }: Props) {
       <ul className={styles.list}>
         {subCategories.map(({ title }, index) => (
           <li
-            className={`${styles.item} ${index === 0 && styles['is-active']}`}
+            className={`${styles.item} ${
+              index === active && styles['is-active']
+            }`}
             key={title}
+            onClick={() => handleActiveTabClick(index)}
           >
             {title}
           </li>
