a8d2576d54fdd9ac8cc6328d26e0c83d43492b0d
presentKey <kim.hyeonjae4@gmail.com>
2023-08-06 23:17:32 +0900

    rename: Headline -> TapPanel 컴포넌트로 이름 변경

--- src/app/(menu)/bookmark/page.tsx
+++ src/app/(menu)/bookmark/page.tsx
@@ -1,11 +1,11 @@
 import SavedBookmark from '@/components/BookmarkPage/SavedBookmark';
-import Headline from '@/components/common/Headline/Headline';
+import TabPanel from '@/components/common/TapPanel/TapPanel';
 import styles from './page.module.css';
 
 export default function BookmarkPage() {
   return (
     <div className={styles.container}>
-      <Headline title='북마크' />
+      <TabPanel title='북마크' />
       <SavedBookmark />
     </div>
   );
--- src/components/common/TapPanel/TapPanel.tsx
+++ src/components/common/TapPanel/TapPanel.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { dohyeon } from '@/utils/fonts';
-import styles from './css/Headline.module.css';
+import styles from './css/TapPanel.module.css';
 import { useRef, useEffect, useCallback, useMemo } from 'react';
 import useTabScroll from '@/recoil/SubCategoryTab/useTabScroll';
 import { throttle } from 'lodash';
@@ -9,16 +9,9 @@ type Props = {
   title: string;
 };
 
-export default function Headline({ title }: Props) {
+export default function TabPanel({ title }: Props) {
   const headRef = useRef<HTMLHeadingElement>(null);
   const { tabLable, scrollToTabPanel, savePanelPosition } = useTabScroll();
-
-  useEffect(() => {
-    if (tabLable === title) {
-      const position = headRef.current?.getBoundingClientRect().top;
-      scrollToTabPanel(position ?? 0);
-    }
-  }, [tabLable, title, scrollToTabPanel]);
 
   const throttleHandler = useMemo(
     () =>
@@ -26,7 +19,6 @@ export default function Headline({ title }: Props) {
         if (headRef.current) {
           const position =
             window.scrollY + headRef.current.getBoundingClientRect().top;
-
           savePanelPosition(title, position);
         }
       }, 700),
@@ -36,10 +28,17 @@ export default function Headline({ title }: Props) {
   const detectTabPanelPositon = useCallback(throttleHandler, [throttleHandler]);
 
   useEffect(() => {
+    if (tabLable === title) {
+      const position = headRef.current?.getBoundingClientRect().top;
+      scrollToTabPanel(position ?? 0);
+    }
+  }, [tabLable, title, scrollToTabPanel]);
+
+  useEffect(() => {
     detectTabPanelPositon();
   }, [detectTabPanelPositon]);
 
-  // 스크롤 이벤트 위치: <TabScrollEvent> 컴포넌트
+  // 스크롤 이벤트의 위치는 <TabScrollEvent> 컴포넌트
   useEffect(() => {
     window.addEventListener('resize', detectTabPanelPositon);
     return () => {
