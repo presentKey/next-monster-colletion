5ec6edc4507f5c3801b62b00b543e4058d105b72
presentKey <kim.hyeonjae4@gmail.com>
2023-08-07 22:46:52 +0900

    feat: CloseButton 컴포넌트 분리

--- src/components/layout/TimerBar/TimerBar.tsx
+++ src/components/layout/TimerBar/TimerBar.tsx
@@ -2,11 +2,11 @@
 import BackgroundOverlay from '@/components/common/BackgroundOverlay/BackgroundOverlay';
 import styles from './css/TimerBar.module.css';
 import useTimerBar from '@/recoil/TimerBar/useTimerBar';
-import CloseIcon from '@/components/common/icons/CloseIcon';
 import useTimerList from '@/recoil/TimerBar/useTimerList';
 import Timer from './Timer';
 import TimerIcon from '@/components/common/icons/TimerIcon';
 import Badge from '@/components/common/Badge/Badge';
+import CloseButton from '@/components/common/CloseButton/CloseButton';
 
 export default function TimerBar() {
   const { open, toggleTimerBar } = useTimerBar();
@@ -18,13 +18,7 @@ export default function TimerBar() {
         <aside className={`${styles.timerbar} ${open && styles['is-open']}`}>
           <header className={styles.header}>
             <h3>타이머</h3>
-            <button
-              className={styles.close}
-              type='button'
-              onClick={toggleTimerBar}
-            >
-              <CloseIcon />
-            </button>
+            <CloseButton onClick={toggleTimerBar} />
           </header>
 
           <ul className={styles.list}>
