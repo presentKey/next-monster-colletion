d931edc6e90d3b0d680c2bbb10ca01591c75379f
presentKey <kim.hyeonjae4@gmail.com>
2023-08-08 03:33:40 +0900

    feat: not-found 페이지 구현

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+import styles from './not-found.module.css';
+
 export default function GlobalNotFound() {
-  return <div>Global Not found</div>;
+  return (
+    <div className={styles.container}>
+      <span className={styles.text}>페이지를 찾을 수 없습니다.</span>
+      <Link className={styles.link} href='/'>
+        홈으로 돌아가기
+      </Link>
+    </div>
+  );
 }
