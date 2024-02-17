fcc86615b7f5ebd3b7ba587832186db094d845d7
presentKey <kim.hyeonjae4@gmail.com>
2024-02-17 21:57:08 +0900

    refactor: TOC IntersectionObserver 적용

--- src/components/routes/category/layout/TableOfContents/index.tsx
+++ src/components/routes/category/layout/TableOfContents/index.tsx
@@ -4,14 +4,47 @@ import styles from './css/index.module.css';
 import TabItem from './TabItem';
 import TabScrollEvent from '@/components/routes/category/layout/TableOfContents/TabScrollEvent';
 import useActiveTabScroll from './hooks/useActiveTabScroll';
+import { useEffect, useRef, useState } from 'react';
 
 type Props = {
   subCategories: SubCategory[];
 };
 
 export default function TableOfContents({ subCategories }: Props) {
-  const { tabRef, active, handleScrollTabClick, saveTabPosition } =
-    useActiveTabScroll();
+  const tabRef = useRef<HTMLElement>(null);
+  const scrollPositionRef = useRef<number>(0);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry, index) => {
+          if (entry.isIntersecting) {
+            const headingEl = entry.target as HTMLHeadingElement;
+
+            setActive(Number(headingEl.dataset.index));
+            scrollPositionRef.current = window.scrollY;
+          } else {
+            const isScrollingUp =
+              scrollPositionRef.current - window.scrollY > 0;
+
+            if (isScrollingUp) setActive((prev) => prev - 1);
+          }
+        });
+      },
+      {
+        rootMargin: '-88px 0px -80% 0px', // 48px(글로벌 헤더 높이) + 40px (윗 여백)
+      }
+    );
+
+    const hadingElements = document.querySelectorAll('.toc-heading');
+
+    hadingElements.forEach((element) => observer.observe(element));
+
+    return () => observer.disconnect();
+  }, []);
+
+  const { handleScrollTabClick, saveTabPosition } = useActiveTabScroll();
 
   return (
     <>
