110dc4c7aa292d3ab99630ead5117535ef375501
presentKey <kim.hyeonjae4@gmail.com>
2024-02-28 02:52:19 +0900

    style: tooltip z-index 및 다크모드 스타일 변경

--- src/components/common/Tooltip/index.tsx
+++ src/components/common/Tooltip/index.tsx
@@ -2,5 +2,5 @@ import { ITooltip, Tooltip as ReactTooltip } from 'react-tooltip';
 import styles from './css/index.module.css';
 
 export default function Tooltip({ ...options }: ITooltip) {
-  return <ReactTooltip {...options} className={styles.darkmode} />;
+  return <ReactTooltip {...options} className={`${styles.tooltip}`} />;
 }
