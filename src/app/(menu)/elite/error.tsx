a46fbf2027dc1bf1f9da704dcc08a7e0ff7e368a
presentKey <kim.hyeonjae4@gmail.com>
2024-02-29 00:44:52 +0900

    feat: elite 페이지 error 컴포넌트 구현

---
+++ src/app/(menu)/elite/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+import ErrorOrNotFound from '@/components/common/ErrorOrNotFound/ErrorOrNotFound';
+import EliteResetButton from '@/components/common/ResetButton/EliteResetButton';
+
+export default function Error() {
+  return (
+    <>
+      <ErrorOrNotFound text='페이지가 존재하지 않거나 오류가 발생했습니다. [E]'>
+        <EliteResetButton />
+      </ErrorOrNotFound>
+    </>
+  );
+}
---
+++ src/components/common/ResetButton/EliteResetButton.tsx
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
+
+import { toast } from 'react-toastify';
+import styles from './css/EliteResetButton.module.css';
+import { resetEliteCollections } from '@/service/request/eliteCollection';
+import { useRouter } from 'next/navigation';
+
+export default function EliteResetButton() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const { mutate: resetMutate } = useMutation(() => resetEliteCollections(), {
+    onSuccess: () => {
+      queryClient.resetQueries(['myCollection', session?.user.uid]);
+      router.push('/');
+    },
+  });
+
+  const handleButtonClick = () => {
+    resetMutate(undefined, {
+      onSuccess: () => toast.success('엘리트 초기화 성공'),
+      onError: () => toast.warn('엘리트 초기화 실패'),
+    });
+  };
+
+  if (!session) return;
+  return (
+    <div className={styles.container}>
+      <span>오류가 계속 발생 시, 엘몬 컬렉션을 초기화해보세요.</span>
+      <button
+        className={styles.button}
+        type='button'
+        onClick={handleButtonClick}
+      >
+        엘리트 초기화
+      </button>
+    </div>
+  );
+}
