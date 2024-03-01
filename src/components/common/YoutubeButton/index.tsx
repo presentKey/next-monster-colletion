f05ac56e971ba6793b0d795c72a4fdede0b10aca
presentKey <kim.hyeonjae4@gmail.com>
2024-03-01 22:26:41 +0900

    refactor: YoutubeButton common 폴더로 이동

--- src/components/common/YoutubeButton/index.tsx
+++ src/components/common/YoutubeButton/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import styles from './css/YoutubeButton.module.css';
+import styles from './css/index.module.css';
 import useYoutube from '@/recoil/Youtube/useYoutube';
 
 type Props = {
